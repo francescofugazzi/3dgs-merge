91a39e7424c0f2399b98f03fce1ecbbbef657368
francescofugazzi <96065025+francescofugazzi@users.noreply.github.com>
2023-10-20 17:16:09 +0200

    Update 3dgsconverter.py
    many fixes and speedups. 3dgs to cc fully working with rgb and density filter flags

--- 3dgsconverter.py
+++ 3dgsconverter.py
@@ -1,8 +1,10 @@
 import argparse
 import numpy as np
+import multiprocessing
 from plyfile import PlyData, PlyElement
 from tqdm import tqdm
 from collections import deque
+from multiprocessing import Pool, cpu_count
 
 def extract_vertex_data(vertices, has_scal=True, has_rgb=False):
     """Extract and convert vertex data from a structured numpy array of vertices."""
@@ -31,6 +33,29 @@ def extract_vertex_data(vertices, has_scal=True, has_rgb=False):
     
     return data
 
+def compute_rgb_from_vertex(plydata, detected_format=None):
+    print("Entering compute_rgb_from_vertex function...")
+    
+    vertices = plydata['vertex'].data
+    print("Loaded vertices data from plydata.")
+
+    if detected_format is None:
+        # If detected format is not provided, infer it from the plydata
+        if 'f_dc_0' in vertices.dtype.names:
+            detected_format = '3dgs'
+        else:
+            detected_format = 'cc'
+    
+    # Depending on the detected format, use the appropriate field names
+    if detected_format == "3dgs":
+        f_dc = np.column_stack((vertices['f_dc_0'], vertices['f_dc_1'], vertices['f_dc_2']))
+    else:
+        f_dc = np.column_stack((vertices['scal_f_dc_0'], vertices['scal_f_dc_1'], vertices['scal_f_dc_2']))
+    
+    colors = (f_dc + 1) * 127.5
+    colors = np.clip(colors, 0, 255).astype(np.uint8)
+    
+    return colors
 
 def define_dtype(has_scal=True, has_rgb=False):
     prefix = 'scal_' if has_scal else ''
@@ -49,68 +74,68 @@ def define_dtype(has_scal=True, has_rgb=False):
     
     return dtype
 
-
 def text_based_detect_format(file_path):
     """Detect if the given file is in '3dgs' or 'cc' format."""
+    print("Entering text_based_detect_format function...")
+    
     with open(file_path, 'rb') as file:
         header_bytes = file.read(2048)  # Read the beginning to detect the format
+        print("Read header bytes from the file...")
 
     header = header_bytes.decode('utf-8', errors='ignore')
+    print("Decoded header to text...")
+
     if "property float f_dc_0" in header:
+        print("Detected format: 3dgs")
         return "3dgs"
     elif "property float scal_f_dc_0" in header:
+        print("Detected format: cc")
         return "cc"
     else:
+        print("No recognized format detected.")
         return None
 
-def convert_3dgs_to_cc_without_rgb(input_path, output_path, plydata):
+def convert_3dgs_to_cc(input_path, output_path, plydata, process_rgb=False):
     """
-    Convert a PLY file from the 3DGS format to the CC format without adding RGB data.
-    This function only updates the header and directly copies the binary data.
+    Convert a PLY file from the 3DGS format to the CC format.
+    This function can optionally add RGB data based on the f_dc values.
     """
     
-    # Get the appropriate dtype for CC format (with "scal_" prefix and without RGB)
-    dtype_for_cc = define_dtype(has_scal=True, has_rgb=False)
-    
-    # Convert the vertices data to CC format
-    converted_data = plydata['vertex'].data.astype(dtype_for_cc)
-    
-    # Write to new PLY file
-    new_plydata = PlyData([PlyElement.describe(converted_data, 'vertex')], byte_order='=')
-    new_plydata.write(output_path)
-    
-    print(f"Converted 3DGS file to CC format without RGB and saved to {output_path}.")
+    print("Starting conversion process...")
+    vertices = plydata['vertex'].data
+    print("Loaded vertices from plydata.")
 
+    if process_rgb:
+        print("Processing RGB data...")
+        # Extract RGB colors from f_dc values and add them to the vertices
+        colors = compute_rgb_from_vertex(plydata)
+        dtype_for_cc = define_dtype(has_scal=True, has_rgb=True)
+        extended_vertices = np.zeros(vertices.shape, dtype=dtype_for_cc)
+        
+        common_fields = set(vertices.dtype.names) & set(extended_vertices.dtype.names)
+        for name in common_fields:
+            extended_vertices[name] = vertices[name]
+        
+        extended_vertices['red'] = colors[:, 0]
+        extended_vertices['green'] = colors[:, 1]
+        extended_vertices['blue'] = colors[:, 2]
 
-def convert_3dgs_to_cc_with_rgb(input_path, output_path, detected_format, vertices=None):
-    """Create a new PLY file with the opposite header of the detected format and binary data from the original."""
+        # Write the new data to the output file
+        print("Writing extended data to the output file...")
+        new_plydata = PlyData([PlyElement.describe(extended_vertices, 'vertex')], byte_order='=')
+        new_plydata.write(output_path)
+    else:
+        print("Processing data without RGB...")
+        # Convert the data directly without adding RGB
+        dtype_for_cc = define_dtype(has_scal=True, has_rgb=False)
+        converted_data = vertices.astype(dtype_for_cc)
+        
+        # Write the converted data to the output file
+        print("Writing converted data to the output file...")
+        new_plydata = PlyData([PlyElement.describe(converted_data, 'vertex')], byte_order='=')
+        new_plydata.write(output_path)
     
-    # If vertices are not provided, read from the input file
-    if vertices is None:
-        plydata = PlyData.read(input_path)
-        vertices = plydata['vertex'].data
-    
-    # Determine if RGB processing is needed
-    assert isinstance(detected_format, str), "detected_format is not a string!"
-    process_rgb = (detected_format == "3dgs")
-    use_scal = (detected_format == "3dgs")
-    
-    data = extract_vertex_data(vertices, has_scal=use_scal, has_rgb=process_rgb)
-    dtype = define_dtype(has_scal=use_scal, has_rgb=process_rgb)
-
-    all_processed_data = []
-
-    # We process in chunks without tqdm
-    for start_idx in range(0, len(vertices), 10000):
-        end_idx = start_idx + 10000
-        chunk_data = data[start_idx:end_idx]
-        all_processed_data.append(chunk_data)
-
-    # Concatenate all processed chunks together
-    final_data = np.concatenate(all_processed_data)
-
-    new_plydata = PlyData([PlyElement.describe(final_data, 'vertex')], byte_order='=')
-    new_plydata.write(output_path)
+    print(f"Converted 3DGS file to CC format {'with' if process_rgb else 'without'} RGB and saved to {output_path}.")
     
 def convert_cc_to_3dgs(input_path, output_path, vertices=None):
     """Create a new 3DGS PLY file from a CC PLY file."""
@@ -132,7 +157,6 @@ def convert_cc_to_3dgs(input_path, output_path, vertices=None):
     new_plydata = PlyData([PlyElement.describe(data, 'vertex')], byte_order='=')
     new_plydata.write(output_path)
 
-
 def get_neighbors(voxel_coords):
     """Get the face-touching neighbors of the given voxel coordinates."""
     x, y, z = voxel_coords
@@ -143,36 +167,45 @@ def get_neighbors(voxel_coords):
     ]
     return neighbors
 
-def apply_density_filter(plydata, voxel_size=1.0, threshold_ratio=0.0032):
-    """
-    Filter the vertices of plydata to keep only those in denser regions and from the largest contiguous voxel cluster.
-    
-    Parameters:
-    - plydata: The PlyData object containing the point cloud.
-    - voxel_size: The size of the voxels used for density calculation.
-    - threshold_ratio: The ratio used to calculate the dynamic threshold. 
-                       Threshold = number of vertices * threshold_ratio
-    """
-    vertices = plydata['vertex'].data
-
-    # Create a dictionary to count points in each voxel
+def count_voxels_chunk(vertices_chunk, voxel_size):
     voxel_counts = {}
-    
-    for vertex in vertices:
-        # Compute the voxel coordinates for each vertex
+    for vertex in vertices_chunk:
         voxel_coords = (int(vertex['x'] / voxel_size), int(vertex['y'] / voxel_size), int(vertex['z'] / voxel_size))
         if voxel_coords in voxel_counts:
             voxel_counts[voxel_coords] += 1
         else:
             voxel_counts[voxel_coords] = 1
+    return voxel_counts
 
-    # Calculate dynamic threshold
+def parallel_voxel_counting(vertices, voxel_size):
+    num_processes = cpu_count()
+    chunk_size = len(vertices) // num_processes
+    chunks = [vertices[i:i + chunk_size] for i in range(0, len(vertices), chunk_size)]
+
+    num_cores = max(1, multiprocessing.cpu_count() - 1)
+    with Pool(processes=num_cores) as pool:
+        results = pool.starmap(count_voxels_chunk, [(chunk, voxel_size) for chunk in chunks])
+
+    # Aggregate results from all processes
+    total_voxel_counts = {}
+    for result in results:
+        for k, v in result.items():
+            if k in total_voxel_counts:
+                total_voxel_counts[k] += v
+            else:
+                total_voxel_counts[k] = v
+
+    return total_voxel_counts
+
+def apply_density_filter(plydata, voxel_size=1.0, threshold_ratio=0.0032):
+    vertices = plydata['vertex'].data
+
+    # Parallelized voxel counting
+    voxel_counts = parallel_voxel_counting(vertices, voxel_size)
+
     threshold = int(len(vertices) * threshold_ratio)
-
-    # Filter out voxels based on density threshold
     dense_voxels = {k: v for k, v in voxel_counts.items() if v >= threshold}
 
-    # Identify the largest contiguous voxel cluster
     visited = set()
     max_cluster = set()
     for voxel in dense_voxels:
@@ -190,16 +223,9 @@ def apply_density_filter(plydata, voxel_size=1.0, threshold_ratio=0.0032):
             if len(current_cluster) > len(max_cluster):
                 max_cluster = current_cluster
 
-    # Filter vertices to retain only those from the largest contiguous voxel cluster
     filtered_vertices = [vertex for vertex in vertices if (int(vertex['x'] / voxel_size), int(vertex['y'] / voxel_size), int(vertex['z'] / voxel_size)) in max_cluster]
-
-    # Update the plydata object with the filtered vertices
     new_vertex_element = PlyElement.describe(np.array(filtered_vertices, dtype=vertices.dtype), 'vertex')
     plydata.elements = (new_vertex_element,) + plydata.elements[1:]
-        
-    print(f"Number of vertices after density filter: {len(filtered_vertices)}")
-
-
 
 def main():
     parser = argparse.ArgumentParser(description="Convert between standard 3D Gaussian Splat and 3D Gaussian Splat for Cloud Compare formats.")
@@ -227,8 +253,7 @@ def main():
         return
 
     plydata = PlyData.read(args.input)
-    vertices = plydata['vertex'].data
-    print(f"Initial number of vertices: {len(vertices)}")
+    print(f"Initial number of vertices: {len(plydata['vertex'].data)}")
 
     if args.density_filter:
         print("Applying density filter...")
@@ -237,22 +262,15 @@ def main():
 
     # Conversion operations
     if detected_format == "3dgs" and args.format == "cc":
-        if args.rgb:
-            convert_3dgs_to_cc_with_rgb(args.input, args.output, vertices)
-        else:
-            convert_3dgs_to_cc_without_rgb(args.input, args.output,  plydata)
+        print("Converting from 3DGS to CC format...")
+        convert_3dgs_to_cc(args.input, args.output, plydata, args.rgb)
     elif detected_format == "cc" and args.format == "3dgs":
-        convert_cc_to_3dgs(args.input, args.output, vertices)
-    elif detected_format == args.format:
-        if args.rgb and detected_format == "3dgs":
-            print("Warning: RGB flag is not supported for 3DGS format. Ignoring RGB flag.")
-        convert_3dgs_to_cc_without_rgb(args.input, args.output, vertices)
+        print("Converting from CC to 3DGS format...")
+        convert_cc_to_3dgs(args.input, args.output, plydata['vertex'].data)
     else:
-        print(f"Invalid conversion direction specified for the detected format.")
+        print(f"Conversion direction not recognized or not supported.")
 
     print(f"Conversion completed. Output saved to: {args.output}")
 
 if __name__ == "__main__":
     main()
-
-
